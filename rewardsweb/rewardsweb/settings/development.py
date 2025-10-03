6dc5a54c2bfdcdde796123bdc32c5803a6c73b23
Ivica Paleka <ipaleka@asastats.com>
2025-10-03 12:05:00 +0200

    Added DATABASE setting for postgreSQL

--- rewardsweb/rewardsweb/settings/development.py
+++ rewardsweb/rewardsweb/settings/development.py
@@ -18,23 +18,23 @@ MIDDLEWARE.insert(2, "debug_toolbar.middleware.DebugToolbarMiddleware")
 
 # # NOTE: db-based tests can't be (yet) done using database setting
 #         involving "service" and "passfile" options
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": get_env_variable("DATABASE_NAME"),
-#         "USER": get_env_variable("DATABASE_USER"),
-#         "PASSWORD": get_env_variable("DATABASE_PASSWORD"),
-#         "HOST": "127.0.0.1",
-#         "PORT": "",  # '5432',
-#     }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR.parent / "fixtures" / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": get_env_variable("DATABASE_NAME"),
+        "USER": get_env_variable("DATABASE_USER"),
+        "PASSWORD": get_env_variable("DATABASE_PASSWORD"),
+        "HOST": "127.0.0.1",
+        "PORT": "",  # '5432',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR.parent / "fixtures" / 'db.sqlite3',
+#     }
+# }
 
 STATICFILES_DIRS = [
     BASE_DIR.parent / "static",

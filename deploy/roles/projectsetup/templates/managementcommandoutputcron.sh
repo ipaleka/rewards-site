a4cf25abc0cd8bbbf7b0d0ff25c5d9c478ed4a68
Ivica Paleka <ipaleka@asastats.com>
2025-10-27 22:21:20 +0100

    Added testing setting file

--- deploy/roles/projectsetup/templates/managementcommandoutputcron.sh
+++ deploy/roles/projectsetup/templates/managementcommandoutputcron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-output="$(export SECRET_KEY='{{ secret_key }}';export DATABASE_NAME={{ env_name }};export DATABASE_USER={{ env_name }};export DATABASE_PASSWORD='{{ db_pwd }}';cd {{ site_path }}/source/{{ app_name }}/;{{ site_path }}/venv/bin/python manage.py {{ item.name }} --settings={{ django_settings }})"
+output="$(export SECRET_KEY='{{ global_env_vars['SECRET_KEY'] }}';export DATABASE_NAME={{ global_env_vars['DATABASE_NAME'] }};export DATABASE_USER={{ global_env_vars['DATABASE_USER'] }};export DATABASE_PASSWORD='{{ global_env_vars['DATABASE_PASSWORD'] }}';cd {{ site_path }}/source/{{ app_name }}/;{{ site_path }}/venv/bin/python manage.py {{ item.name }} --settings={{ django_settings }})"
 if [[ {{ item.output }} ]] ; then
     exit 0
 else
